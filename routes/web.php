6c30b13d8428993d86b99de1ba79a26c54b08814
Ndoghwe01 <nathanndoghwe@gmail.com>
2025-06-05 09:05:26 +0300

    Changes made in route file

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,33 @@
 <?php
-
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('web')->group(function () {
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
+    Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+    Route::post('/register', [RegisterController::class, 'register']);
+
+    Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+    Route::post('/login', [LoginController::class, 'login']);
+    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+    Route::post('/home/profile', [HomeController::class, 'updateProfile'])->name('profile.update');
+
+    Route::post('/cart/add', [CartController::class, 'add'])->name('cart.add');
+    Route::delete('/cart/remove/{id}', [CartController::class, 'remove'])->name('cart.remove');
+    Route::get('/cart/count', [CartController::class, 'count'])->name('cart.count');
+    Route::post('/cart/update', [CartController::class, 'update'])->name('cart.update');
+
+    Route::post('/payment/checkout', [PaymentController::class, 'checkout'])->name('payment.checkout');
+    Route::get('/payment/success', [PaymentController::class, 'success'])->name('paypal.success');
+    Route::get('/payment/cancel', [PaymentController::class, 'cancel'])->name('paypal.cancel');
 });
+?>
